e5ab0dcb2e7e640bf3862aad48333cb135c18870
Your Name <you@example.com>
2022-02-06 15:35:45 -0300

    aula 13 maps runner steps

---
+++ src/test/java/steps/GenericSteps.java
@@ -0,0 +1,13 @@
+package steps;
+
+import io.cucumber.java.pt.Entao;
+
+public class GenericSteps {
+    @Entao("valido que recebo o status {int} no response")
+    public void validoQueReceboOStatusNoResponse(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
